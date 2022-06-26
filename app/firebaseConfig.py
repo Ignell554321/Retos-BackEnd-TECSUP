0f81516b628c9909905cd2758f99b07cd41ffc87
unknown <miguelguevara_94@outlook.com>
2022-06-25 21:51:28 -0500

    update reto 2

---
+++ app/firebaseConfig.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyAN5JaU7mMk5vjfH98gVtBzKx1qNmNG098",
+  "authDomain": "portaafolio-g15.firebaseapp.com",
+  "projectId": "portaafolio-g15",
+  "storageBucket": "portaafolio-g15.appspot.com",
+  "messagingSenderId": "981109203328",
+  "appId": "1:981109203328:web:03f7e33f78cf80f5b62165",
+  "measurementId": "G-9BDXG1V1DV",
+  "databaseURL":""
+}
---
+++ app/portafolio/views.py
@@ -0,0 +1,53 @@
+from flask import Flask,render_template,request,session,flash,redirect,url_for
+
+from .forms import ContactoForm
+
+from . import portafolio
+
+from app import fb
+
+@portafolio.route('/')
+def index():
+    dicBiografia = fb.getCollection('biografia')[0]
+   
+    session['biografia'] = dicBiografia
+    return render_template('portafolio/index.html')
+
+@portafolio.route('/proyectos')
+def proyectos():
+    listaProyectos = fb.getCollection('proyectos')
+    
+    context = {
+        'proyectos':listaProyectos
+    }
+
+    return render_template('portafolio/portafolio.html',**context)
+
+@portafolio.route('/acercade')
+def acercade():
+    return render_template('portafolio/acercade.html')
+
+@portafolio.route('/contacto',methods=['GET','POST'])
+def contacto():
+
+    contacto_form = ContactoForm()
+    context = {
+        'contacto_form':contacto_form
+    }
+
+    if contacto_form.validate_on_submit():
+
+        dataNuevoContacto = {
+                'email' : contacto_form.email.data,
+                'mensaje' : contacto_form.mensaje.data,
+                'nombre' : contacto_form.nombre.data
+            }
+        print(dataNuevoContacto)
+
+        nuevoContacto = fb.insertDocument('contactos',dataNuevoContacto)
+       
+        flash("Se ha enviado el mensaje")
+        return redirect(url_for('portafolio.contacto'))
+       
+        
+    return render_template('portafolio/contacto.html',**context)
